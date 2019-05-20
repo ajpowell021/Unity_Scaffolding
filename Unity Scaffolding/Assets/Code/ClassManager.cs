cd3a3c55f31a7e85c5e471222a5ceb1325f9b88a
ajpowell021 <ajpowell021@gmail.com>
2019-05-20 00:13:48 -0400

    Add input manager and class manager.

---
+++ Unity Scaffolding/Assets/Code/ClassManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassManager : MonoBehaviour {
+    
+    // Instance
+
+    public static ClassManager instance = null;
+
+    // Classes
+
+    public InputManager inputManager;
+    
+    // Init
+
+    private void Awake() {
+        setInstance();
+        setClasses();
+    }
+    
+    // Private Functions
+
+    private void setInstance() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != null) {
+            Destroy(this);
+        }
+    }
+
+    private void setClasses() {
+        inputManager = gameObject.GetComponent<InputManager>();
+    }
+}
---
+++ Unity Scaffolding/Assets/Code/InputManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    
+    // Update
+
+    private void Update() {
+        // Check for inputs here.
+    }
+}
